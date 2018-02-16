e457547da2d588e1f01fc5db821b67802c495f1d
Jaap van Blaaderen <jbl@woodwing.com>
2018-02-16 09:19:15 +0100

    Massive code cleanup for publish panel

--- elvis-plugins/publications/resources/publications.js
+++ elvis-plugins/publications/resources/publications.js
@@ -1,55 +1,34 @@
 (function() {
+  const MAX_STATS_RESULT_COUNT = 10000;
+  const MAX_ASSET_COUNT = 900;
+ 
   var elvisApi;
   var elvisContext;
-  var selectedValues = [];
+
+  var selectedFilterValues = [];
   var selectedHits;
-  var MAX_ASSET_COUNT = 900;
-
-  function selectionUpdated() {
-    if (!elvisContext || !parent) {
-      // Plugin is no longer active
-      return;
-    }
-    selectedHits = elvisContext.activeTab.originalAssetSelection;
-    populatePublishedInPanel();
-    showCorrectPanel();
-  }
-
-  function populatePublishedInPanel() {
-    if (selectedHits.length !== 1) {
-      $('#publishedIn').html('<div class="#invalidSelection">Select one file</div>');
-      return;
-    }
-    $.ajax({
-      type: 'GET',
-      url: '/plugins/stats_api/publish/stats/' + selectedHits[0].id,
-      contentType: 'application/json; charset=utf-8',
-      dataType: 'json',
-      success: function (response) {
-
-        var html = response.map((hit) => {
-          //TODO: cleanup
-          var itemHtml = '<div class="statRecord">'
-            + '<div class="statField"><span class="statFieldLabel">Publication date:</span> <span class="statFieldValue">' + hit.published + '</span></div>'
-            + '<div class="statField"><span class="statFieldLabel">Brand:</span> <span class="statFieldValue">' + hit.brand + '</span></div>';
-          if (hit.target) {
-            itemHtml += '<div class="statField"><span class="statFieldLabel">Target:</span> <span class="statFieldValue">' + hit.target + '</span></div>';
-          }
-          if (hit.issue) {
-            itemHtml += '<div class="statField"><span class="statFieldLabel">Issue:</span> <span class="statFieldValue">' + hit.issue + '</span></div>';
-          }
-          itemHtml += '</div>';
-          return itemHtml;
-        }).join('');
-        $('.publishedIn').html(html);
-      },
-      error: function (jqXHR, textStatus, error) {
-        console.log('API request failed: ' + JSON.stringify(jqXHR));
-      }
+  
+  var entityMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
+  };
+  
+  function escapeHtml (string) {
+    return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+      return entityMap[s];
     });
   }
 
-  function populateSearchFields() {
+  /**
+   * Initialize date search components
+   */
+  function initDateFields() {
     $.datepicker.setDefaults({
       dateFormat: 'dd-mm-yy',
       dayNamesMin: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
@@ -61,24 +40,36 @@
     $('#endDate').datepicker();
   }
 
+  /**
+   * Event handler for facet selection changes
+   */
   function search() {
-    loadCategories(true);
+    searchStats(true);
   }
 
-  function loadCategories(searchAssets) {
-    var containsQuery = false;
+  /**
+   * Retrieve publication facets and optionally retrieve stats hits
+   * 
+   * @param retrieveHits Set to true to retrieve stats hits 
+   */
+  function searchStats(retrieveHits) {
+    
+    // Gather search params
+    var isFiltered = false;
     var params = {};
     var paramNames = ['startDate', 'endDate', 'brand', 'issue', 'target'];
     for (paramName of paramNames) {
       var value = $('#' + paramName).val();
       if (value) {
-        containsQuery = true;
+        isFiltered = true;
         params[paramName] = value;
       }
     }
 
-    params.size = containsQuery ? 10000 : 0;
+    // Size 0 will ensure only facets are returned (which is relevant when we don't have any filter selected)
+    params.size = isFiltered ? MAX_STATS_RESULT_COUNT : 0;
 
+    // Query stats API
     $.ajax({
       type: 'GET',
       url: '/plugins/stats_api/publish/statsSearch',
@@ -86,8 +77,8 @@
       dataType: 'json',
       data: params,
       success: function (response) {
-        if (searchAssets) {
-          executeSearch(response.hits, containsQuery);
+        if (retrieveHits) {
+          showHitsInClients(response.hits, !isFiltered);
         }
         showFacets(response.facets);
       },
@@ -97,48 +88,121 @@
     });
   }
 
-  function executeSearch(hits, containsQuery) {
-    var query = '';
-    if (containsQuery) {
-      if (hits.length == 0) {
-        hits[0] = { assetId: 'nothingfound' };
-      }
-      var uniqueAssetIds = [];
-      var i=0;
-      while (uniqueAssetIds.length < MAX_ASSET_COUNT && i < hits.length) {
-        if (uniqueAssetIds.indexOf(hits[i].assetId) == -1) {
-          uniqueAssetIds.push(hits[i].assetId);
-        }
-        i++;
-      }
-      console.log("Hit count: " + hits.length + "; Unique asset count: " + uniqueAssetIds.length);
-      query = 'id:' + uniqueAssetIds.join(' OR id:');
+  /**
+   * Gathers unique asset id's in the given statsHits and shows 
+   * these assets in the web client content pane.
+   * 
+   * @param hits Stats hits to show
+   * @param clearSearch No filter selected, clear the search
+   */
+  function showHitsInClients(statsHits, clearSearch) {
+    if (clearSearch) {
+      elvisContext.openSearch('');
+      return;
     }
-    // console.log('query = ' + query)
+    if (statsHits.length == 0) {
+      // TODO: Not a really nice way to say we didn't find anything...
+      elvisContext.openSearch('assetId:"nothingfound"');
+      return;
+    }
+    
+    var uniqueAssetIds = [];
+    var i=0;
+    while (uniqueAssetIds.length < MAX_ASSET_COUNT && i < statsHits.length) {
+      if (uniqueAssetIds.indexOf(statsHits[i].assetId) == -1) {
+        uniqueAssetIds.push(statsHits[i].assetId);
+      }
+      i++;
+    }
+    var query = 'id:' + uniqueAssetIds.join(' OR id:');
     elvisContext.openSearch(query);
   }
 
+  /**
+   * Display facets for brand, issue and target
+   * 
+   * @param facets All stats facets
+   */
   function showFacets(facets) {
     var selectedBrandIdx = fillSelect('brand', facets.brand);
     fillSelect('issue', selectedBrandIdx > -1 ? facets.issue : []);
     fillSelect('target', selectedBrandIdx > -1 ? facets.target : []);
   }
 
-  function fillSelect(name, facets) {
+  /**
+   * Create facet options in SELECT
+   * 
+   * @param name Name of the SELECT element
+   * @param facetFields Array of facetField objects
+   */
+  function fillSelect(name, facetFields) {
     var options = '<option></option>\n';
     var selectedIdx = -1;
-    for (var i=0; i<facets.length; i++) {
-      var facetField = facets[i];
+    for (var i=0; i<facetFields.length; i++) {
+      var facetField = facetFields[i];
       var facet = facetField[name] ? facetField[name] : facetField;
-      options += '<option value="' + facet.name + '"';
-      if (facet.name === selectedValues[name]) {
+      options += '<option value="' + escapeHtml(facet.name) + '"';
+      if (facet.name === selectedFilterValues[name]) {
         selectedIdx = i;
         options += ' selected';
       }
-      options += '>' + facet.name + ' (&asymp;' + facet.count + ')</option>';
+      options += '>' + escapeHtml(facet.name) + ' (&asymp;' + facet.count + ')</option>';
     }
     $('#' + name).html(options);
     return selectedIdx;
+  }
+
+  /**
+   * Selection change handler that acts when the asset selection changes
+   */
+  function selectionUpdated() {
+    if (!elvisContext || !parent) {
+      // Plugin is no longer active
+      return;
+    }
+    selectedHits = elvisContext.activeTab.originalAssetSelection;
+    renderDetailPanel();
+    showCorrectPanel();
+  }
+
+  /**
+   * Show stats details for the currently seledted panel
+   */
+  function renderDetailPanel() {
+    if (selectedHits.length !== 1) {
+      $('#publishedIn').html('<div class="#invalidSelection">Select one file</div>');
+      return;
+    }
+    $.ajax({
+      type: 'GET',
+      url: '/plugins/stats_api/publish/stats/' + selectedHits[0].id,
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      success: function (response) {
+        var html = response.map((hit) => {
+          return '<div class="statRecord">'
+            + renderFieldValue('Publication date', hit.published)
+            + renderFieldValue('Brand', hit.brand)
+            + renderFieldValue('Target', hit.target)
+            + renderFieldValue('Issue', hit.issue)
+            + '</div>';
+        }).join('');
+        $('.publishedIn').html(html);
+      },
+      error: function (jqXHR, textStatus, error) {
+        console.log('API request failed: ' + JSON.stringify(jqXHR));
+      }
+    });
+  }
+
+  /**
+   * Render a single stats field-value pair
+   */
+  function renderFieldValue(field, value) {
+    if (!value) {
+      return '';
+    }
+    return '<div class="statField"><span class="statFieldLabel">' + escapeHtml(field) + ':</span> <span class="statFieldValue">' + escapeHtml(value) + '</span></div>';
   }
 
   function showCorrectPanel(forceOpenSearchPanel) {
@@ -160,33 +224,38 @@
     $(className).css('display', 'none');
   }
 
+  /**
+   * Initialize everything after DOM loaded
+   */
   $(function () {
     elvisContext = ElvisPlugin.resolveElvisContext();
+    elvisContext.updateCallback = selectionUpdated;
     elvisApi = new ElvisAPI("${serverUrl}");
 
+    initDateFields();
+
+    // Add event handlers for inputs, when an input changes, perform a search
     $('#startDate').change(search);
     $('#endDate').change(search);
     $('#brand').change(function () {
-      selectedValues['brand'] = $(this).val();
-      selectedValues['issue'] = '';
-      selectedValues['target'] = '';
+      selectedFilterValues['brand'] = $(this).val();
+      selectedFilterValues['issue'] = '';
+      selectedFilterValues['target'] = '';
       search();
     });
     $('#issue').change(function () {
-      selectedValues['issue'] = $(this).val();
+      selectedFilterValues['issue'] = $(this).val();
       search();
     });
     $('#target').change(function () {
-      selectedValues['target'] = $(this).val();
+      selectedFilterValues['target'] = $(this).val();
       search();
     });
     $('.backToSearch').click(function () {
       showCorrectPanel(true);
     });
 
-    elvisContext.updateCallback = selectionUpdated;
     selectionUpdated();
-    populateSearchFields();
-    loadCategories(false);
+    searchStats(false);
   });
 })();
